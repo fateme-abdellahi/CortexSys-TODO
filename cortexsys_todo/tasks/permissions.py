03e497a21a46a3348978ff8fc8cb1d0157ce8731
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-09-04 18:02:35 +0330

    add permissions to tasks

--- cortexsys_todo/tasks/permissions.py
+++ cortexsys_todo/tasks/permissions.py
@@ -1,12 +1,13 @@
 from rest_framework import permissions
 
+
 class IsTaskOwnerOrAdmin(permissions.BasePermission):
     """
-    Custom permission to only allow owner of the task or admins to view, create, update, or delete it.
+    Custom permission to allow only owner of the task or admins to view, create, update, or delete it.
     """
-    
+
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
-    
+
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user or request.user.is_staff
